e2b4aa0b9cf1c01cd3f73f17c6ef25d673da8776
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-22 11:43:48 +0000

    Solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    strList = ["a","b","c","d"]
+    print(strList)
+    print(remove_last(strList))
 
+def remove_last(strings):
+  print(strings)
+  lastRemoved = []
+  for i in range((len(strings))):
+    if(i==len(strings)-1):
+      strings.pop(i)
+      return strings
+      
 if __name__ == '__main__':
     main()
